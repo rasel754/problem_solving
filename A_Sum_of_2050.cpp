d303abea60f7bc446f5437fa594609d37d885894
unknown <raselahmed73614@gmail.com>
2025-11-04 23:28:17 +0600

    sum of 2050 for cp

---
+++ A_Sum_of_2050.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        long long int n;
+        cin >> n;
+
+        int ans = 0;
+
+        while (n >= 2050)
+        {
+            ans++;
+            long long int x = 2050;
+            while (x <= n)
+            {
+                x *= 10;
+            }
+            x /= 10;
+            n = n - x;
+        }
+        if (n != 0)
+            cout << -1 << endl;
+        else
+            cout << ans << endl;
+    }
+
+    return 0;
+}
